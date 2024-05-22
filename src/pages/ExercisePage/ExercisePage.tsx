de7ed6078fb838b2195d952479652b732495e48e
Igor21v <igor21v@mail.ru>
2024-05-22 15:37:53 +0800

    feat: add animation and vibration when wrong answer

--- src/pages/ExercisePage/ExercisePage.tsx
+++ src/pages/ExercisePage/ExercisePage.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef, useState} from 'react';
-import {Animated, StyleSheet, TouchableOpacity} from 'react-native';
+import {Animated, Easing, StyleSheet, TouchableOpacity, Vibration} from 'react-native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../../shared/types/route';
 import {NumKeyboard} from './NumKeyboard';
@@ -11,12 +11,21 @@ export const ExercisePage = ({route, navigation}: Props) => {
   const [ans, setAns] = useState('?');
   const [task, setTask] = useState({firstNum: 0, secondNum: 0});
   const [error, setError] = useState(false);
+  useEffect(() => {
+    genTask(20);
+  }, []);
 
+  // Анимация ошибки
   let value = useRef(new Animated.Value(0)).current;
   const startAnimate = () => {
-    Animated.timing(value, {toValue: 1.2, useNativeDriver: true}).start();
+    Animated.timing(value, {
+      toValue: 3,
+      useNativeDriver: true,
+      easing: Easing.elastic(10),
+    }).start();
   };
 
+  // Генерим задачу
   const genTask = (max: number) => {
     const firstNum = Math.floor(Math.random() * (max - 1)) + 1;
     const secondMax = max - firstNum;
@@ -24,10 +33,7 @@ export const ExercisePage = ({route, navigation}: Props) => {
     setTask({firstNum: firstNum, secondNum: secondNum});
   };
 
-  useEffect(() => {
-    genTask(20);
-  }, []);
-
+  // Проверка ответа
   const check = () => {
     if (task.firstNum + task.secondNum === +ans) {
       setError(false);
@@ -37,20 +43,20 @@ export const ExercisePage = ({route, navigation}: Props) => {
       setError(true);
       value.setValue(0);
       startAnimate();
+      Vibration.vibrate(80);
     }
   };
 
   return (
     <>
-      <Animated.View style={[styles.help, {opacity: value}]}>
-        <TouchableOpacity>
-          <AppText size="s">Нужна помощь?</AppText>
-        </TouchableOpacity>
+      <TouchableOpacity style={styles.help}>
+        <AppText size="s">Нужна помощь?</AppText>
+      </TouchableOpacity>
+      <Animated.View style={[styles.task, {transform: [{translateX: value}]}]}>
+        <AppText size="l" error={error}>
+          {task.firstNum}+{task.secondNum}={ans}
+        </AppText>
       </Animated.View>
-
-      <AppText size="l" error={error} style={styles.task}>
-        {task.firstNum}+{task.secondNum}={ans}
-      </AppText>
       <NumKeyboard setNum={setAns} enter={check} setError={setError} />
     </>
   );
@@ -64,7 +70,6 @@ const styles = StyleSheet.create({
     marginLeft: 'auto',
     flexDirection: 'row',
     alignItems: 'center',
-    marginTop: 4,
-    marginRight: 4,
+    padding: 8,
   },
 });
