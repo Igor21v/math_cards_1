9570bd946f142f68a6a2b37aeb480e66698fb2df
Igor Bondarenko <igor21v@mail.ru>
2024-06-09 21:26:12 +0800

    build: add generic type to getStorage

--- src/shared/lib/getStorage.ts
+++ src/shared/lib/getStorage.ts
@@ -1,8 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const getStorage = async (key: string, fn: (value: any) => void) => {
+export const getStorage = async <T>(key: string, fn: (value: T) => void) => {
   try {
-    const value = await AsyncStorage.getItem(key);
+    const value = (await AsyncStorage.getItem(key)) as T;
     fn(value);
   } catch (e) {
     console.log(e);
